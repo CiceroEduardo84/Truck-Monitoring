e233ae1ff736a1369fdb26e8bc3b50db751d9955
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-06-04 22:18:58 -0300

    add defoult env types, update server and fuction routers

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
+    SECRET_TOKEN: string;
+    EXPIRESIN_TOKEN: string;
+    KEY_TOKEN: string;
+    CLIENT_SIDE: string;
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,34 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import { pageNotFound } from "./errors/pageNotFound";
+import { appErrors } from "./errors/appErrors";
+import { routers } from "./routes";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
+
+const whiteList = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  process.env.CLIENT_SIDE,
+];
+
+app.use(
+  cors({
+    origin: whiteList,
+    credentials: true,
+  })
+);
+app.use(routers);
+
+app.use(pageNotFound);
+app.use(appErrors);
+
 app.listen(PORT, () => {
   console.log(`Server running in the PORT: http://localhost:${PORT}/`);
 });
