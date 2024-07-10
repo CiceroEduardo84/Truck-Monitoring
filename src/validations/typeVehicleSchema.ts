eb7f97cfa441c7561aac02d4c9dff7cc3b8ccd93
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-07-09 23:29:48 -0300

    feat: add to strict in the validation type vehicles

--- src/validations/typeVehicleSchema.ts
+++ src/validations/typeVehicleSchema.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
-export const typeVehicleSchema = z.object({
-  name: z
-    .string({
-      required_error: "Name is required!",
-      invalid_type_error: "Name must be a string!",
-    })
-    .min(3, "Name must have at least 3 characters!")
-    .max(255, "Max name length exceeded!"),
-});
+export const typeVehicleSchema = z
+  .object({
+    name: z
+      .string({
+        required_error: "Name is required!",
+        invalid_type_error: "Name must be a string!",
+      })
+      .min(3, "Name must have at least 3 characters!")
+      .max(255, "Max name length exceeded!"),
+  })
+  .strict();
 
 export type TypeVehicleSchema = z.infer<typeof typeVehicleSchema>;
