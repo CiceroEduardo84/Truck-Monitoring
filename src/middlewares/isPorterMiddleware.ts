72da628d5a043f27045d1f0300076ae426287d05
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-07-28 08:21:17 -0300

    refactor: decouple validation status in the middleware

--- src/middlewares/isPorterMiddleware.ts
+++ src/middlewares/isPorterMiddleware.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { userRepository } from "../repositories/userRepository";
 import { appError } from "../errors/appError";
-import { vehicleSchema } from "../validations/vehicleSchema";
 
 export async function isPorterMiddleware(
   req: Request,
@@ -10,14 +9,13 @@ export async function isPorterMiddleware(
 ) {
   try {
     const id = req.userID;
-    const { status } = vehicleSchema.parse(req.body);
     const user = await userRepository.getUserByID(id);
 
     if (!user) throw appError("Usuário não encontrado", 404);
-    if (user.type != "admin" && user.type != "porter")
+    if (user.type != "admin" && user.type != "porter") {
       throw appError("Usuário não autorizado", 401);
-    if (user.type == "porter" && status == "loading")
-      throw appError("Vehiculo não cadastrado, status não autorizado!", 401);
+    }
+
     return next();
   } catch (error) {
     return next(error);
