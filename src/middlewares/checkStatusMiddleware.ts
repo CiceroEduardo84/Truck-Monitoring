252de6607a23e0483ddfc7eaf9daf127fa271a34
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-07-28 08:16:53 -0300

    feat: add check status middleware un the routes create and update vehicle

---
+++ src/middlewares/checkStatusMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { userRepository } from "../repositories/userRepository";
+import { appError } from "../errors/appError";
+import { vehicleSchema } from "../validations/vehicleSchema";
+
+export async function checkStatusMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const id = req.userID;
+    const { status } = vehicleSchema.parse(req.body);
+    const user = await userRepository.getUserByID(id);
+
+    if (!user) throw appError("Usuário não encontrado", 404);
+    if (user.type == "porter" && status == "loading") {
+      throw appError("Vehiculo não cadastrado, status não autorizado!", 401);
+    }
+    if (user.type == "dispatcher" && status != "loading") {
+      throw appError("Vehiculo não cadastrado, status não autorizado!", 401);
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+}
