cff0adf3d23cf08f045e8718202423cbac5a2d24
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-07-28 08:17:17 -0300

    feat: add check status middleware un the routes create and update vehicle

--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { vehicleControllers } from "../controllers/vehicleControllers";
 import { isAdminMiddleware } from "../middlewares/isAdminMiddleware";
 import { isPorterMiddleware } from "../middlewares/isPorterMiddleware";
+import { checkStatusMiddleware } from "../middlewares/checkStatusMiddleware";
 
 export const vehicleRoutes = Router();
-vehicleRoutes.post("/vehicle", isPorterMiddleware, vehicleControllers.create);
+vehicleRoutes.post("/vehicle", isPorterMiddleware, checkStatusMiddleware, vehicleControllers.create);
 vehicleRoutes.get("/vehicle", vehicleControllers.read);
-vehicleRoutes.put("/vehicle/:id", vehicleControllers.update);
+vehicleRoutes.put("/vehicle/:id",checkStatusMiddleware, vehicleControllers.update);
 
 vehicleRoutes.delete("/vehicle/:id", isAdminMiddleware, vehicleControllers.delete);
