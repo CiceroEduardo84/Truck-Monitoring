0f3d4df1ca2a3953696330d0be2d73108b28cbab
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-08-08 23:29:20 -0300

    feat: update pool connections

--- src/databases/postgreSQL/index.ts
+++ src/databases/postgreSQL/index.ts
@@ -1,16 +1,13 @@
-import { Client, Pool } from "pg";
 import "dotenv/config";
+import { Pool } from "pg";
 
-export async function postgreSqlConnection() {
-  const db = new Pool({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-  });
-
-  const client = await db.connect();
-  
-  return client;
-}
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
