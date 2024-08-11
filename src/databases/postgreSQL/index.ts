9be729cd4bbc16cda47fc7785b79d182f3113615
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-08-11 00:06:53 -0300

    feat: update connection pool for seed and table creation.

--- src/databases/postgreSQL/index.ts
+++ src/databases/postgreSQL/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { Pool } from "pg";
 
-const pool = new Pool({
+export const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
--- src/databases/postgreSQL/migrations/seed.ts
+++ src/databases/postgreSQL/migrations/seed.ts
@@ -1,20 +1,20 @@
-import { postgreSqlConnection } from "..";
 import { randomUUID } from "node:crypto";
 import { userAdmin } from "../../../configs/userAdmin";
 import { hash } from "bcrypt";
+import { pool } from "..";
 
 export async function createAdminUser() {
   try {
+    const client = await pool.connect();
+
     if (!userAdmin.email || !userAdmin.password) {
       throw new Error(
         "As variáveis de ambiente ADMIN_EMAIL ou ADMIN_PASSWORD não estão definidas"
       );
     }
 
-    const db = await postgreSqlConnection();
-
     const quarySelect = "SELECT * FROM users WHERE email = $1;";
-    const res = await db.query(quarySelect, [userAdmin.email]);
+    const res = await client.query(quarySelect, [userAdmin.email]);
 
     if (res.rows[0]) {
       console.error("Usuário admin já existe!");
@@ -25,7 +25,7 @@ export async function createAdminUser() {
     const passwordHash = await hash(userAdmin.password, 10);
     const quaryInsert =
       "INSERT INTO users(id_user, name, email, password, type) VALUES($1, $2, $3, $4, $5);";
-    const result = await db.query(quaryInsert, [
+    const result = await client.query(quaryInsert, [
       id,
       userAdmin.name,
       userAdmin.email,
@@ -34,6 +34,8 @@ export async function createAdminUser() {
     ]);
 
     console.log("Usuário admin criado com sucesso!", result.rowCount);
+
+    client.release();
   } catch (error) {
     console.error("Erro ao criar usuário admin:", error);
   }
